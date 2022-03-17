791b6212294b3672c3abccda20b60fae85742f05
Tomas Delclaux <dlxtomas@gmail.com>
2022-03-17 12:55:06 -0400

    project todos defined

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+const vector<int> INPUT;
+
+// TODO BRUTE FORCE CONTIGUOUS SUBARRAY 1D
+vector<int> bruteForce(vector<int> input){}
+
+// TODO DP CONTIGUOUS SUBARRAY 1D (square)
+vector<int> maxContSubArr(vector<int> input){}
+
+// TODO DP CONTIGUOUS SUBARRAY 1D (linear)
+vector<int> betterMaxContSubArr(vector<int> input){}
+
+
+// TODO BRUTE FORCE CONTIGUOUS SUBARRAY 2D
+vector<vector<int>> bruteForce(vector<vector<int>> input){}
+
+// TODO DP CONTIGUOUS SUBARRAY 2D (pow 4)
+vector<vector<int>> maxSubRect(vector<vector<int>> input){}
+
+// TODO DP CONTIGUOUS SUBARRAY 1D (pow 3)
+vector<vector<int>> betterMaxSubRect(vector<vector<int>> input){}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
